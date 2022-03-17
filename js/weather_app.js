0625f0f39ba8bcb7fb2cff5f3c6114ee50798ac3
Samuel Arguello <sam.rene12@gmail.com>
2022-03-17 16:53:01 -0500

    completed details card section and added notes all files

--- js/weather_app.js
+++ js/weather_app.js
@@ -12,8 +12,9 @@ $.get("https://api.openweathermap.org/data/2.5/onecall?", {
     lat:    lat,
     lon:   long,
     units: "imperial"
-
+// on data population display weather detail and forecast cards
 }).done(function(data) {
+    $("#details").html(createDetailCard(data));
     $("#forecast").html(createWeatherTables(data));
 });
 
@@ -28,13 +29,10 @@ const map = new mapboxgl.Map({
 });
 
 
-
-
-
 // ========== Search Functionality ==========
 // button on click sets location based on the city entered within the input tag
 $("button").click(function() {
-
+// geocode function takes in input from search bar and moves map to city location and updates lat/long
     geocode($("#location").val(), MAPBOX_API_TOKEN).then(function (result) {
         map.setCenter(result);
         map.setZoom(10);
@@ -51,9 +49,10 @@ $("button").click(function() {
             lon:   long,
             units: "imperial"
 
-// creates the weather tables based on location
+// creates the details and forecast tables based on location and displays marker on location
         }).done(function(data) {
             $("#forecast").html(createWeatherTables(data));
+            $("#details").html(createDetailCard(data));
             displayCityData(marker._lngLat.lat, marker._lngLat.lng);
         });
     });
@@ -63,14 +62,52 @@ $("button").click(function() {
 
 
 
-// ========== Create Weather Tables Function ==========
-// creates weather cards and displays them to the dom
-function createWeatherTables(data) {
+// ========== Functions ==========
+// function displays detailed weather info card on page
+function createDetailCard(data){
     let html = '';
 
+    let date = new Date(data.current.dt * 1000).toDateString();
+    let time = new Date(data.current.dt * 1000).toTimeString();
+    let sunRise = new Date(data.current.sunrise * 1000).toLocaleTimeString();
+    let sunSet = new Date(data.current.sunset * 1000).toLocaleTimeString();
+
+    html += `<div class="detail-card">   
+                <h3>${date}</h3>
+                <h5>${time}</h5>
+                    <hr>
+                <img id="daily-img" src="http://openweathermap.org/img/w/${data.current.weather[0].icon}.png">
+                <p>${data.current.feels_like}&#176</p>
+                <p>${data.current.weather[0].description}</p>
+                    <hr>
+                <table>
+                    <tr>
+                        <td>Todays High: ${data.daily[0].temp.max}&#176</td>
+                        <td>Todays Low: ${data.daily[0].temp.min}&#176</td>
+                    </tr>
+                    <tr>
+                        <td>Sunrise: ${sunRise}</td>
+                        <td>Sunset: ${sunSet}</td>
+                    </tr>
+                    <tr>
+                        <td>Wind Direction: ${windCardinalDirection(data.current.wind_deg)}</td>
+                        <td>Wind Speed: ${data.current.wind_speed}mph</td>
+                    </tr>
+                    <tr>
+                        <td>Humidity: ${data.current.humidity}%</td>
+                        <td>UV Index: ${data.current.uvi}</td>
+                    </tr>
+                </table>
+            </div>`
+
+    return html;
+}
+
+// creates 5-day forecast cards and displays them on the page
+function createWeatherTables(data) {
+    let html = '';
 // creates 5 duplicate cards with different data populated by the API
     for (let i = 0; i < 5; i++) {
-
 // takes in date data in the API and displays it in a more readable format
         let date = new Date(data.daily[i].dt * 1000).toDateString();
 
@@ -85,8 +122,52 @@ function createWeatherTables(data) {
     return html;
 }
 
+// function takes in the wind direction(in degrees) and converts them to a directional output
+function windCardinalDirection(degrees){
+    let cardinalDirection = '';
+    if ((degrees > 348.75 && degrees <= 360) || (degrees >=0 && degrees <= 11.25)){
+        cardinalDirection = "N";
+    } else if (degrees > 11.25 && degrees  <= 33.75) {
+        cardinalDirection = "NNE";
+    } else if (degrees > 33.75 && degrees <= 56.25) {
+        cardinalDirection = "NE";
+    } else if (degrees > 56.25 && degrees <= 78.75) {
+        cardinalDirection = "ENE";
+    } else if (degrees > 78.75 && degrees <= 101.25) {
+        cardinalDirection = "E";
+    } else if (degrees > 101.25 && degrees <= 123.75) {
+        cardinalDirection = "ESE";
+    } else if (degrees > 123.75 && degrees <= 146.25) {
+        cardinalDirection = "SE";
+    } else if (degrees > 146.25 && degrees <= 168.75) {
+        cardinalDirection = "SSE";
+    } else if (degrees > 168.75 && degrees <= 191.25) {
+        cardinalDirection = "S";
+    } else  if (degrees > 191.25 && degrees <= 213.75) {
+        cardinalDirection = "SSW";
+    } else if (degrees > 213.75 && degrees <= 236.25)  {
+        cardinalDirection = "SW";
+    } else if (degrees > 236.25 && degrees <= 258.75) {
+        cardinalDirection = "WSW";
+    } else if (degrees > 258.75 && degrees <= 281.25) {
+        cardinalDirection = "W";
+    } else if (degrees > 281.25 && degrees <= 303.75) {
+        cardinalDirection = "WNW";
+    } else if (degrees > 303.75 && degrees <= 326.25) {
+        cardinalDirection = "NW";
+    } else if (degrees > 326.75 && degrees <= 348.75) {
+        cardinalDirection = "NNW";
+    }
+    return cardinalDirection;
+}
 
-
+// takes in lat/lng and displays the address of wherever the marker is placed, originally will populate in San Antonio
+function displayCityData(lat, lng){
+    reverseGeocode({lat, lng}, MAPBOX_API_TOKEN).then(function(results) {
+        $("#current-city").html(results)
+        console.log(results)
+    })
+}
 
 
 // ========== Map Marker ==========
@@ -103,17 +184,10 @@ marker.on("dragend", function(){
         lat:   marker._lngLat.lat,
         lon:   marker._lngLat.lng,
         units: "imperial"
-
+// once data is loaded it is displayed in the details and forecast cards
     }).done(function(data) {
+        $("#details").html(createDetailCard(data));
         $("#forecast").html(createWeatherTables(data));
         displayCityData(marker._lngLat.lat, marker._lngLat.lng);
     })
 })
-
-
-function displayCityData(lat, lng){
-    reverseGeocode({lat, lng}, MAPBOX_API_TOKEN).then(function(results) {
-        $("#current-city").html(results)
-        console.log(results)
-    })
-}
